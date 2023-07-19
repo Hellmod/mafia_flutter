41d92be98cfa1ed3cc3f3ebec6f53fec54d92c4b
rmiskiewicz <rafalmiskiewicz9@gmal.com>
2023-07-19 19:32:49 +0200

    game choose button in HomeScrean

--- lib/heomeScreanButton.dart
+++ lib/heomeScreanButton.dart
@@ -1,136 +1,122 @@
 // custom_container.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeScreanButton extends StatelessWidget {
   final String title;
   final String body;
   final String details;
-  final double marginBottom;
   final String icon;
 
   HomeScreanButton(
       {required this.title,
       required this.body,
       required this.details,
-      required this.marginBottom,
       required this.icon});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // buttonFcD (154:18175)
-      margin: EdgeInsets.fromLTRB(0, 0, 0, marginBottom),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Color(0x7f595959),
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: const [
-          BoxShadow(
-            color: Color(0x192f2b43),
-            offset: Offset(0, 1),
-            blurRadius: 1.5,
+    return InkWell(
+        onTap: () {
+          Fluttertoast.showToast(
+              msg: "Trwają prace nad funkcjonalnością",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 1,
+              backgroundColor: Colors.grey,
+              textColor: Colors.white,
+              fontSize: 16.0);
+        },
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(0, 12, 0, 0),
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: const Color(0x7f595959),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: const [
+              BoxShadow(
+                color: Color(0x192f2b43),
+                blurRadius: 2,
+              ),
+            ],
           ),
-        ],
-      ),
-      child: Container(
-        // frame427320270KMB (154:18176)
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              // frame427320275UE5 (154:18177)
-              margin: EdgeInsets.fromLTRB(0, 0, 0, 12),
-              width: 40,
-              height: 40,
-              child: SvgPicture.asset(
-                icon,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 12),
                 width: 40,
                 height: 40,
+                decoration: const BoxDecoration(
+                  color: Color(0x1AFFFFFF),
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: SvgPicture.asset(
+                    icon,
+                    width: 24,
+                    height: 24,
+                  ),
+                ),
               ),
-            ),
-            Container(
-              // frame427320276mys (154:18180)
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+                child: Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: 'Clash Display Variable',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    height: 1.3,
+                    color: Color(0xffffffff),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 3),
+                child: Text(
+                  body,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontFamily: 'Inter',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    height: 1.5999999728,
+                    color: Color(0xffa3a3a3),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Center(
-                    //RM część
-                    // moderowanarozgrywkaXTF (154:18181)
-                    child: Container(
-                      margin: const EdgeInsets.fromLTRB(0, 0, 0, 4),
-                      child: Text(
-                        //RM
-                        title,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontFamily: 'Clash Display Variable',
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          height: 1.3,
-                          color: Color(0xffffffff),
-                        ),
-                      ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                    width: 16,
+                    height: 16,
+                    child: SvgPicture.asset(
+                      'assets/images/Users.svg',
+                      width: 16,
+                      height: 16,
                     ),
                   ),
-                  Container(
-                    //RM część
-                    // obecnogamemasteraRYd (154:18182)
-                    margin: EdgeInsets.fromLTRB(0, 0, 0, 3),
-                    child: Text(
-                      body,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontFamily: 'Inter',
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        height: 1.5999999728,
-                        color: Color(0xffa3a3a3),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    // frame427320302jpD (154:18183)
-                    margin: EdgeInsets.fromLTRB(51, 0, 51, 0),
-                    width: double.infinity,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          // users5NH (154:18184)
-                          margin: EdgeInsets.fromLTRB(0, 0, 8, 1),
-                          width: 16,
-                          height: 16,
-                          child: SvgPicture.asset(
-                            'assets/images/Users.svg',
-                            width: 16,
-                            height: 16,
-                          ),
-                        ),
-                        Text(
-                          // graczy1Fw (154:18185)
-                          details,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontFamily: 'Inter',
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            height: 1.5999999728,
-                            color: Color(0xffa3a3a3),
-                          ),
-                        ),
-                      ],
+                  Text(
+                    details,
+                    style: const TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      height: 1.5,
+                      color: Color(0xffa3a3a3),
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+import 'heomeScreanButton.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -62,7 +63,15 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
+        body: /*HomeScreanButton(
+          title: "Moderowana rozgrywka",
+          body: "Obecność Game Mastera",
+          details: "4-16 graczy",
+          marginBottom: 12,
+          icon: "assets/images/SmileyWink.png",
+        ),*/
+
+        Container(
             decoration: const BoxDecoration(
               color: Color(0xff1e1e1e),
             ),
@@ -78,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       image: AssetImage("assets/images/card.png"),
                     ),
                   ),
-                ),
+                ),//Karta
                 BackdropFilter(
                   filter: ImageFilter.blur(
                     sigmaX: 9,
@@ -91,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                   ),
-                ),
+                ),//rozmazanie Karty
                 Positioned(
                   top: 52.0,
                   right: 16.0,
@@ -105,20 +114,87 @@ class _MyHomePageState extends State<MyHomePage> {
                             timeInSecForIosWeb: 1,
                             backgroundColor: Colors.grey,
                             textColor: Colors.white,
-                            fontSize: 16.0
-                        );
+                            fontSize: 16.0);
                       },
                       child: Container(
                         width: 44,
                         height: 44,
                         decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white, width: 1), // Border o szerokości 1px
-                          borderRadius: BorderRadius.circular(12), // Dodaje zaokrąglenie 12px
+                          border: Border.all(color: Colors.white, width: 1),
+                          // Border o szerokości 1px
+                          borderRadius: BorderRadius.circular(
+                              12), // Dodaje zaokrąglenie 12px
                         ),
                         child: SvgPicture.asset(
                           'assets/images/UserCircle.svg',
                         ),
                       ),
+                    ),
+                  ),
+                ),//UserButton
+                Positioned(
+                  // frame427320286FRs (154:18166)
+                  left: 18,
+                  right: 18,
+                  top: 104,
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: const Text(
+                            'Mafia',
+                            style: TextStyle(
+                              fontFamily: 'Clash Display Variable',
+                              fontSize: 32,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: -0.64,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                        ), //MAFIA
+                        Container(
+                          // wybierztyprozgrywkiyub (154:18173)
+                          margin: const EdgeInsets.fromLTRB(0, 12, 0, 0),
+                          child: const Text(
+                            "Wciel się w rolę jednego z członków mafii lub mieszkańca miasta, który za wszelką cenę chce pozbyć się gangsterów ",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xffA4A4A4),
+                            ),
+                          ),
+                        ),//Description
+                        Container(
+                          // wybierztyprozgrywkiyub (154:18173)
+                          margin: const EdgeInsets.fromLTRB(0, 64, 0, 0),
+                          child: const Text(
+                            'Wybierz typ rozgrywki:',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                        ),//Zachęta
+                        HomeScreanButton(
+                          title: "Moderowana rozgrywka",
+                          body: "Obecność Game Mastera",
+                          details: "4-16 graczy",
+                          icon: "assets/images/SmileyWink.svg",
+                        ),
+                        HomeScreanButton(
+                          title: "Brak moderacji",
+                          body:
+                          "Rolę Game Mastera przejmuje aplikacja",
+                          details: "4-16 graczy",
+                          icon: "assets/images/DeviceMobile.svg",
+                        ),
+                      ],
                     ),
                   ),
                 ),
