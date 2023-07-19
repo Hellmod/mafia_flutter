08d8ed58281158319431840c3620960f4cda1e8c
rmiskiewicz <rafalmiskiewicz9@gmal.com>
2023-07-19 23:07:25 +0200

    facebook button in HomeScrean

--- lib/main.dart
+++ lib/main.dart
@@ -71,136 +71,170 @@ class _MyHomePageState extends State<MyHomePage> {
           icon: "assets/images/SmileyWink.png",
         ),*/
 
-        Container(
-            decoration: const BoxDecoration(
-              color: Color(0xff1e1e1e),
-            ),
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  width: 319,
-                  height: 411,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/card.png"),
-                    ),
-                  ),
-                ),//Karta
-                BackdropFilter(
-                  filter: ImageFilter.blur(
-                    sigmaX: 9,
-                    sigmaY: 9,
-                  ),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      gradient: RadialGradient(
-                        colors: <Color>[Color(0xcc2f404b), Color(0xcc161a22)],
-                      ),
-                    ),
-                  ),
-                ),//rozmazanie Karty
-                Positioned(
-                  top: 52.0,
-                  right: 16.0,
-                  child: Align(
-                    child: InkWell(
-                      onTap: () {
-                        Fluttertoast.showToast(
-                            msg: "Trwają prace nad funkcjonalnością",
-                            toastLength: Toast.LENGTH_SHORT,
-                            gravity: ToastGravity.BOTTOM,
-                            timeInSecForIosWeb: 1,
-                            backgroundColor: Colors.grey,
-                            textColor: Colors.white,
-                            fontSize: 16.0);
-                      },
-                      child: Container(
-                        width: 44,
-                        height: 44,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white, width: 1),
-                          // Border o szerokości 1px
-                          borderRadius: BorderRadius.circular(
-                              12), // Dodaje zaokrąglenie 12px
-                        ),
-                        child: SvgPicture.asset(
-                          'assets/images/UserCircle.svg',
-                        ),
-                      ),
-                    ),
-                  ),
-                ),//UserButton
-                Positioned(
-                  // frame427320286FRs (154:18166)
-                  left: 18,
-                  right: 18,
-                  top: 104,
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          child: const Text(
-                            'Mafia',
-                            style: TextStyle(
-                              fontFamily: 'Clash Display Variable',
-                              fontSize: 32,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: -0.64,
-                              color: Color(0xffffffff),
-                            ),
-                          ),
-                        ), //MAFIA
-                        Container(
-                          // wybierztyprozgrywkiyub (154:18173)
-                          margin: const EdgeInsets.fromLTRB(0, 12, 0, 0),
-                          child: const Text(
-                            "Wciel się w rolę jednego z członków mafii lub mieszkańca miasta, który za wszelką cenę chce pozbyć się gangsterów ",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontFamily: 'Inter',
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xffA4A4A4),
-                            ),
-                          ),
-                        ),//Description
-                        Container(
-                          // wybierztyprozgrywkiyub (154:18173)
-                          margin: const EdgeInsets.fromLTRB(0, 64, 0, 0),
-                          child: const Text(
-                            'Wybierz typ rozgrywki:',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontFamily: 'Inter',
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xffffffff),
-                            ),
-                          ),
-                        ),//Zachęta
-                        HomeScreanButton(
-                          title: "Moderowana rozgrywka",
-                          body: "Obecność Game Mastera",
-                          details: "4-16 graczy",
-                          icon: "assets/images/SmileyWink.svg",
-                        ),
-                        HomeScreanButton(
-                          title: "Brak moderacji",
-                          body:
-                          "Rolę Game Mastera przejmuje aplikacja",
-                          details: "4-16 graczy",
-                          icon: "assets/images/DeviceMobile.svg",
-                        ),
-                      ],
-                    ),
-                  ),
+            Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xff1e1e1e),
                 ),
-              ],
-            )
-        )
-    );
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: 319,
+                      height: 411,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/card.png"),
+                        ),
+                      ),
+                    ), //Karta
+                    BackdropFilter(
+                      filter: ImageFilter.blur(
+                        sigmaX: 9,
+                        sigmaY: 9,
+                      ),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          gradient: RadialGradient(
+                            colors: <Color>[
+                              Color(0xcc2f404b),
+                              Color(0xcc161a22)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ), //rozmazanie Karty
+                    Positioned(
+                      top: 52.0,
+                      right: 16.0,
+                      child: Align(
+                        child: InkWell(
+                          onTap: () {
+                            Fluttertoast.showToast(
+                                msg: "Trwają prace nad funkcjonalnością",
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.BOTTOM,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.grey,
+                                textColor: Colors.white,
+                                fontSize: 16.0);
+                          },
+                          child: Container(
+                            width: 44,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white, width: 1),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: SvgPicture.asset(
+                              width: 24,
+                              height: 24,
+                              'assets/images/UserCircle.svg',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ), //UserButton
+                    Positioned(
+                      // frame427320286FRs (154:18166)
+                      left: 18,
+                      right: 18,
+                      top: 104,
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: const Text(
+                                'Mafia',
+                                style: TextStyle(
+                                  fontFamily: 'Clash Display Variable',
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: -0.64,
+                                  color: Color(0xffffffff),
+                                ),
+                              ),
+                            ), //MAFIA
+                            Container(
+                              // wybierztyprozgrywkiyub (154:18173)
+                              margin: const EdgeInsets.fromLTRB(0, 12, 0, 0),
+                              child: const Text(
+                                "Wciel się w rolę jednego z członków mafii lub mieszkańca miasta, który za wszelką cenę chce pozbyć się gangsterów ",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xffA4A4A4),
+                                ),
+                              ),
+                            ), //Description
+                            Container(
+                              // wybierztyprozgrywkiyub (154:18173)
+                              margin: const EdgeInsets.fromLTRB(0, 64, 0, 0),
+                              child: const Text(
+                                'Wybierz typ rozgrywki:',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xffffffff),
+                                ),
+                              ),
+                            ), //Zachęta
+                            HomeScreanButton(
+                              title: "Moderowana rozgrywka",
+                              body: "Obecność Game Mastera",
+                              details: "4-16 graczy",
+                              icon: "assets/images/SmileyWink.svg",
+                            ),
+                            HomeScreanButton(
+                              title: "Brak moderacji",
+                              body: "Rolę Game Mastera przejmuje aplikacja",
+                              details: "4-16 graczy",
+                              icon: "assets/images/DeviceMobile.svg",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 32.0,
+                      right: 16.0,
+                      child: InkWell(
+                        onTap: () {
+                          Fluttertoast.showToast(
+                              msg: "Trwają prace nad funkcjonalnością",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.grey,
+                              textColor: Colors.white,
+                              fontSize: 16.0);
+                        },
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 12),
+                          width: 40,
+                          height: 40,
+                          decoration: const BoxDecoration(
+                            color: Color(0x1AFFFFFF),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                          child: Center(
+                            child: SvgPicture.asset(
+                              'assets/images/Facebook.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ), //FacebookButton
+                  ],
+                )));
   }
 }
