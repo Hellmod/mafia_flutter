e70c592d446927bf606580325753833691b5e115
rmiskiewicz <rafalmiskiewicz9@gmal.com>
2023-07-20 17:46:07 +0200

    simple navigation

---
+++ lib/screans/lobby_one_phone_add_user_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LobbyAddUserScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Lobby Add User Screen'),
+    );
+  }
+}
