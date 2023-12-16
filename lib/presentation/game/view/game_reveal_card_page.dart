89dd091d3c281031a2cc3e7c0d124fb0bf584359
ramisk <rafal.miskiewicz@assaabloy.com>
2023-12-16 02:17:10 +0100

    loader card

--- lib/presentation/game/view/game_reveal_card_page.dart
+++ lib/presentation/game/view/game_reveal_card_page.dart
@@ -8,6 +8,7 @@ import '../../../utils/Wigets.dart';
 import '../../../utils/character/CharacterRepository.dart';
 import '../../wigets/RoleCard.dart';
 import '../block/game_bloc.dart';
+import 'dart:math' as math;
 
 class GameRevealCard extends StatefulWidget {
   final List<Character> characters;
@@ -18,18 +19,34 @@ class GameRevealCard extends StatefulWidget {
   _GameRevealCard createState() => _GameRevealCard(characters);
 }
 
-class _GameRevealCard extends State<GameRevealCard> {
+class _GameRevealCard extends State<GameRevealCard>
+    with SingleTickerProviderStateMixin {
   GameBloc? myBloc;
+  late AnimationController controller;
 
   final List<Character> characters;
   String userNick = '';
 
   _GameRevealCard(this.characters);
 
+  void startLoading() {
+    controller.forward().then((_) {
+      onLoadingComplete();
+    });
+  }
+
+  void onLoadingComplete() {
+    print("Loading complete!");
+  }
+
   @override
   void initState() {
     super.initState();
     myBloc = context.read<GameBloc>();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    )..repeat();
   }
 
   @override
@@ -94,78 +111,119 @@ class _GameRevealCard extends State<GameRevealCard> {
 
   Widget card() => AspectRatio(
         aspectRatio: 4 / 6,
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Color(0xFF1E1E1E),
-            borderRadius: BorderRadius.circular(24.0),
-            border: Border.all(
-              color: Colors.white.withOpacity(0.2),
-              width: 16.0,
-            ),
-          ),
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Color(0xFF1E1E1E),
-                borderRadius: BorderRadius.circular(24.0),
-                border: Border.all(
-                  color: Colors.white.withOpacity(0.2),
-                  width: 1.0,
-                ),
+        child: GestureDetector(
+          onTap: startLoading, // Rozpoczęcie ładowania po kliknięciu
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color(0xFF1E1E1E),
+              borderRadius: BorderRadius.circular(24.0),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.2),
+                width: 16.0,
               ),
-              child: Stack(
-                fit: StackFit.expand, // Zmień StackFit.loose na StackFit.expand
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      // Dodano, aby rozłożyć dzieci równomiernie
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      // Zmieniono na CrossAxisAlignment.start
-                      children: [
-                        const Text(
-                          'Mafia',
-                          style: AppTextStyles.text_C_14_600,
-                        ),
-                        Center(
-                            child: Column(children: [
-                          Container(
-                            decoration: const BoxDecoration(
-                              color: Color(0xFF4b4b4b),
-                              shape: BoxShape.circle,
-                            ),
-                            padding: EdgeInsets.all(8),
-                            child: const Icon(
-                              Icons.fingerprint,
-                              size: 32,
-                              color: Colors.white,
-                            ),
-                          ),
-                          const SizedBox(height: 16),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xFF1E1E1E),
+                  borderRadius: BorderRadius.circular(24.0),
+                  border: Border.all(
+                    color: Colors.white.withOpacity(0.2),
+                    width: 1.0,
+                  ),
+                ),
+                child: Stack(
+                  fit: StackFit.expand,
+                  // Zmień StackFit.loose na StackFit.expand
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        // Dodano, aby rozłożyć dzieci równomiernie
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // Zmieniono na CrossAxisAlignment.start
+                        children: [
                           const Text(
-                            'Naciśnij i przytrzymaj,\naby odsłonić rolę',
-                            textAlign: TextAlign.center,
-                            style: AppTextStyles.text12_500,
-                          ),
-                        ])),
-                        const Align(
-                          alignment: Alignment.bottomRight,
-                          child: Text(
                             'Mafia',
                             style: AppTextStyles.text_C_14_600,
                           ),
-                        ),
-                      ],
+                          Center(
+                              child: Column(children: [
+                            CustomPaint(
+                              painter: CircleProgressPainter(controller),
+                              child: Container(
+                                width: 64,
+                                // Ustawienie szerokości kontenera
+                                height: 64,
+                                // Ustawienie wysokości kontenera
+                                decoration: const BoxDecoration(
+                                  color: Color(0xFF4b4b4b),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: const Icon(
+                                  Icons.fingerprint,
+                                  size: 32,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            const Text(
+                              'Naciśnij i przytrzymaj,\naby odsłonić rolę',
+                              textAlign: TextAlign.center,
+                              style: AppTextStyles.text12_500,
+                            ),
+                          ])),
+                          const Align(
+                            alignment: Alignment.bottomRight,
+                            child: Text(
+                              'Mafia',
+                              style: AppTextStyles.text_C_14_600,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ),
       );
+}
+
+class CircleProgressPainter extends CustomPainter {
+  final Animation<double> animation;
+
+  CircleProgressPainter(this.animation) : super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.orange
+      ..strokeWidth = 8
+      ..style = PaintingStyle.stroke;
+
+    double progress = (1.0 - animation.value) * 2 * math.pi;
+    canvas.drawArc(
+      Rect.fromCircle(
+          center: Offset(size.width / 2, size.height / 2),
+          radius: size.width / 2),
+      -math.pi / 2,
+      progress,
+      false,
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
 }
